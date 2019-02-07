b129875a4178921b2e1cc21ca36a3d754695fe23
Tiago <t.dfgomes@icloud.com>
2019-02-07 23:17:21 +0000

    refactor: use asyn await

--- api/src/app.js
+++ api/src/app.js
@@ -2,18 +2,16 @@ const assert = require('assert')
 const createServer = require('./server')
 
 const runApp = async () => {
-  createServer().then(
-    app =>
-      app.listen(9090, err => {
-        assert(!err, err)
-
-        console.log('SERVER RUNNING')
-      }),
-    err => {
-      console.error('Error while starting up server', err)
-      process.exit(1)
-    }
-  )
+  const app = await createServer()
+  try {
+    await app.listen(9090, err => {
+      assert(!err, err)
+      console.log('SERVER RUNNING')
+    })
+  } catch (error) {
+    console.error('Error while starting up server', error)
+    process.exit(1)
+  }
 }
 
 module.exports = runApp
