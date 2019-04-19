6b58beaa23651549197c580fbb1bff1d199b9f75
Tiago <t.dfgomes@icloud.com>
2019-04-19 23:19:26 +0100

    chore: change linting rules

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,64 +1,63 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['standard', 'plugin:react/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": ["react-app", 'plugin:react/recommended'],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/forbid-prop-types': ['error', { forbid: ['any'] }],
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'react/jsx-boolean-value': ['off'],
+    'react/jsx-indent': ['warn', 2],
+    'react/jsx-indent-props': ['warn', 2],
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+    'react/sort-comp': [
+      'error',
+      {
+        order: ['static-methods', 'lifecycle', 'everything-else', 'render'],
+        groups: {
+          lifecycle: [
+            'displayName',
+            'propTypes',
+            'props',
+            'contextTypes',
+            'childContextTypes',
+            'mixins',
+            'statics',
+            'defaultProps',
+            'state',
+            'constructor',
+            'getDefaultProps',
+            'getInitialState',
+            'state',
+            'getChildContext',
+            'componentWillMount',
+            'componentDidMount',
+            'componentWillReceiveProps',
+            'shouldComponentUpdate',
+            'componentWillUpdate',
+            'componentDidUpdate',
+            'componentWillUnmount'
+          ]
+        }
+      }
     ],
-    "rules": {
-        'jsx-quotes': ['error', 'prefer-double'],
-        'react/forbid-prop-types': ['error', { 'forbid': ['any'] }],
-        'no-multiple-empty-lines': ['error', { 'max': 1 }],
-        'react/jsx-boolean-value': ['off'],
-        'react/jsx-indent': ['warn', 2],
-        'react/jsx-indent-props': ['warn', 2],
-        'react/jsx-pascal-case': 'error',
-        'react/jsx-tag-spacing': ['error', { 'beforeSelfClosing': 'always' }],
-        'react/sort-comp': ['error', {
-            'order': [
-                'static-methods',
-                'lifecycle',
-                'everything-else',
-                'render'
-            ],
-            'groups': {
-                'lifecycle': [
-                    'displayName',
-                    'propTypes',
-                    'props',
-                    'contextTypes',
-                    'childContextTypes',
-                    'mixins',
-                    'statics',
-                    'defaultProps',
-                    'state',
-                    'constructor',
-                    'getDefaultProps',
-                    'getInitialState',
-                    'state',
-                    'getChildContext',
-                    'componentWillMount',
-                    'componentDidMount',
-                    'componentWillReceiveProps',
-                    'shouldComponentUpdate',
-                    'componentWillUpdate',
-                    'componentDidUpdate',
-                    'componentWillUnmount'
-                ]
-            }
-        }],
-    }
-};
+    'react/no-unused-prop-types': 'warn',
+    'react/no-multi-comp': ['warn', { ignoreStateless: true }],
+    'react/prefer-stateless-function': ['error', { ignorePureComponents: true }]
+  }
+}
